f56cdabf93d40c085c7d910e0d80ea4e81608e7e
RicardoBatel <ricardo.batel@hotmail.com>
2022-12-22 11:03:52 -0300

    Abstração da classe Routes

--- App/Route.php
+++ App/Route.php
@@ -2,24 +2,11 @@
 
 namespace App;
 
-class Route {
-    
-    private $routes;
+use MF\Init\Bootstrap;
 
-    public function __construct() {
-        $this->initRoutes();
-        $this->run($this->getUrl());
-    }
-
-    public function getRoutes(){
-        return $this->routes;
-    }
-    
-    public function setRoutes(array $routes) {
-        $this->routes = $routes;
-    }
-
-    public function initRoutes() {
+class Route extends Bootstrap {   
+       
+    protected function initRoutes() {
         $routes['home'] = array(
             'route' => '/',
             'controller' => 'indexController',
@@ -33,25 +20,7 @@ class Route {
         );
 
         $this->setRoutes($routes);
-    }
-
-    public function run($url) {        
-        foreach ($this->getRoutes() as $key => $route) {
-            if($url == $route['route']){
-                $class = "App\\Controllers\\".ucfirst($route['controller']);
-
-                $controller = new $class;
-
-                $action = $route['action'];
-
-                $controller->$action();
-            }
-        }
-    }
-    
-    public function getUrl() {
-        return parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-    }
+    } 
 }
 
 ?>
